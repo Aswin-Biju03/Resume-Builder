cb411f055ed3f2b469a6093395488be3a8262644
Aswin-Biju03 <aswinbiju0303@gmail.com>
2026-03-25 01:33:04 +0530

    Server Updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://ai-resume-builder-server-pnd7.onrender.com",
   timeout: 5000,
 });
 
